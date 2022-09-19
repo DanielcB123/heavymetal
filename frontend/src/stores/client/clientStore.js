2aa81b688fb04b14f0d7597f9c36a255f2e0cf44
Daniel Burgess <burgess.daniel4141@gmail.com>
2022-09-18 19:41:26 -0500

    fixed post method to get method in client-search

--- frontend/src/stores/client/clientStore.js
+++ frontend/src/stores/client/clientStore.js
@@ -18,7 +18,7 @@ export const ClientStore = defineStore("client", {
                 console.log("test api array -> " , this.clients);
                 //  console.log("length", this.client.length);
                  axios
-                    .post('http://localhost:8000/api/client/client-search')
+                    .get('http://localhost:8000/api/client/client-search')
                     .then((result) => { 
                         this.clients = result.data;
                     })   
